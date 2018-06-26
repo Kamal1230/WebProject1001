f58039d41f3f4d1273451d6e2fbe92ea77efc95b
Kamal1230 <hossainkamal313@gmail.com>
2018-06-26 13:24:22 -0900

    new file added

---
+++ src/learn_jave/Operator.java
@@ -0,0 +1,36 @@
+package learn_jave;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int result=600;
+		
+		if
+		(result >=661 || result ==660) {
+			System.out.println(" 'congrats' "+" your passing score is First Division.\n\t"
+			+ "\tyou are in Dean list.");
+			
+		}
+		
+		else
+			if (result < 660 && result >=550) {
+				System.out.println(" you are in second Division.");
+			}
+			else {
+				System.out.println("You Barely pass the exam. study Hard");
+			}
+	}
+	}
+	
+
+
+
+
+	
+
+
+
+
